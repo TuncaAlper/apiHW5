80bbb6c0af6f6ccf0fca697f658b2de386c163ae
Bencilki <alpertunca@gmail.com>
2019-03-15 23:50:26 +0100

    authentication part completed. user can signup and login with pass> stored as hash

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const customersRouter = require('./customers/routes')
-const companyRouter = require('./companies/routes')
-const loginRouter = require('./auth/routes')
 const usersRouter = require('./users/routes')
+const tokenRouter = require('./auth/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
 
-// app
-//   .use(bodyParser.json())
-//   .use(customersRouter, companyRouter, loginRouter, usersRouter)
-//   .listen(port, () => console.log(`Listening on port ${port}`))
+app
+  .use(bodyParser.json())
+  .use(usersRouter, tokenRouter)
+  .listen(port, () => console.log(`Listening on port ${port}`))
 
