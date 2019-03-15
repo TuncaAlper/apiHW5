2caf6de14e9c2dee309fb863fca1c1d5e7a42947
Bencilki <alpertunca@gmail.com>
2019-03-15 21:19:37 +0100

    dependencies and index-db.js setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const customersRouter = require('./customers/routes')
+const companyRouter = require('./companies/routes')
+const loginRouter = require('./auth/routes')
+const usersRouter = require('./users/routes')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+// app
+//   .use(bodyParser.json())
+//   .use(customersRouter, companyRouter, loginRouter, usersRouter)
+//   .listen(port, () => console.log(`Listening on port ${port}`))
+
